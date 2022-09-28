263a8dc8dc2215a49cf9750bb054970dedbe2526
swzhxx <549909896@qq.com>
2022-09-28 17:19:11 +0800

    build(build): build

--- types/core/utils/convert.d.ts
+++ types/core/utils/convert.d.ts
@@ -3,4 +3,11 @@ export declare let bodyMeshName: any;
 export declare let Tooth_downMeshName: any;
 export declare function setBodyMorphTargetDictionary(name: any, map: any): void;
 export declare function setTeethMorphTargetDictionary(name: any, map: any): void;
+/**
+ *
+ * @param { object } fp 动画文件json结构的描述
+ * @param {boolean} isEmotion 是否为表情类型的动画
+ * @returns { AnimationClip }
+ * @desc 通过将JSON结构的描述转化为 THREEJS 的AnimationClip
+ */
 export default function Convert(fp: any, isEmotion?: boolean): THREE.AnimationClip;
