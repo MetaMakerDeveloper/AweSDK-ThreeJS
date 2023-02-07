d273fa6199ff2821897e1d3ae9085305f305bda2
swzhxx <549909896@qq.com>
2023-02-07 13:34:15 +0800

    (build):build

--- types/core/index.d.ts
+++ types/core/index.d.ts
@@ -4,7 +4,7 @@ import { downloadAnimation, loadAnimationData } from "./utils/downloadAnimation"
 import Convert from "./utils/convert";
 declare function loadGLTFModel(url: string): Promise<THREE.Group>;
 declare function parseGLTFModel(buffer: ArrayBuffer): Promise<THREE.Group>;
+declare function loadGLTFAnimation(url: string): Promise<THREE.AnimationClip>;
 declare function loadTTSTeethAnimation(url: string): Promise<THREE.AnimationClip>;
 declare function loadTTSEmoAnimation(url: string): Promise<THREE.AnimationClip>;
-declare function loadGLTFAnimation(url: string): Promise<THREE.AnimationClip>
-export { Convert, loadAnimationData, downloadAnimation, loadGLTFModel, parseGLTFModel, loadTTSTeethAnimation, loadTTSEmoAnimation, resetPolygonOffset ,loadGLTFAnimation};
+export { Convert, loadAnimationData, downloadAnimation, loadGLTFModel, parseGLTFModel, loadTTSTeethAnimation, loadTTSEmoAnimation, resetPolygonOffset, loadGLTFAnimation };
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,7 @@
 import * as core from "./core";
+import * as ClothPhysics from "./core/utils/ClothPhysics";
 declare const MMFT: {
     core: typeof core;
+    ClothPhysics: typeof ClothPhysics;
 };
 export default MMFT;
